d28c1af51849e62d9ada8329ce25777d7e822c91
fredrick <fmmbugua@safaricom.co.ke>
2022-07-15 12:03:23 +0300

    Products Model done

--- src/models/Products.js
+++ src/models/Products.js
@@ -5,10 +5,54 @@ module.exports = sequelize.define("Product", {
   id: {
     type: Sequelize.INTEGER(11),
     allowNull: false,
+    primaryKey: true,
     autoIncrement: true,
   },
-  addedBy: Sequelize.STRING(300),
-  productName: Sequelize.STRING(300),
+  productName: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+    unique: true,
+  },
+  productDescription: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  productPicMain: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  productPicTwo: {
+    type: Sequelize.STRING(255),
+  },
+  productPicThree: {
+    type: Sequelize.STRING(255),
+  },
+  productPicFour: {
+    type: Sequelize.STRING(255),
+  },
+  productUnitOfMeasure: {
+    type: Sequelize.STRING(255),
+  },
+  productInstructionsLink: {
+    type: Sequelize.STRING(255),
+  },
+  productVideoLink: {
+    type: Sequelize.STRING(255),
+  },
+  stockStatus: {
+    type: Sequelize.STRING(255),
+  },
+  productStatus: {
+    type: Sequelize.INTEGER(11),
+  },
+  expiryDate: {
+    type: Sequelize.DATE,
+  },
+  productStorageFacility: {
+    type: Sequelize.INTEGER(11),
+  },
+  addedBy: Sequelize.STRING(30),
+  updatedBy: Sequelize.STRING(30),
   createdAt: Sequelize.DATE,
   updatedAt: Sequelize.DATE,
 });
