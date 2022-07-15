bbc0f66f793ebe2932fed68bf9a909794335e380
fredrick <fmmbugua@safaricom.co.ke>
2022-07-15 12:08:07 +0300

    Add users model

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,19 +1,54 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../Database/connection");
 
-module.exports = sequelize.define("User", {
+module.exports = sequelize.define("Users", {
     id: {
         type: Sequelize.INTEGER(11),
         allowNull: false,
+        primaryKey: true,
         autoIncrement: true,
     },
     username: {
-        type: Sequelize.STRING(300),
+        type: Sequelize.STRING(255),
         allowNull: false,
         unique: true,
     },
+    firstName: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+    },
+    lastName: {
+        type: Sequelize.STRING(30),
+    },
+    password: {
+        type: Sequelize.STRING(255),
+    },
+    msisdn: {
+        type: Sequelize.STRING(30),
+    },
+    userRole: {
+        type: Sequelize.STRING(30),
+    },
+    status: {
+        type: Sequelize.INTEGER(11),
+    },
+    emailAddress: {
+        type: Sequelize.STRING(100),
+    },
+    verificationToken: {
+        type: Sequelize.STRING(255),
+    },
+    verificationStatus: {
+        type: Sequelize.INTEGER(11),
+    },
+    verificationTime: {
+        type: Sequelize.DATE,
+    },
+    addedBy: Sequelize.STRING(30),
+    updatedBy: Sequelize.STRING(30),
     createdAt: Sequelize.DATE,
     updatedAt: Sequelize.DATE,
 });
 
 // sequelize migration:create --name create_users_table
+
