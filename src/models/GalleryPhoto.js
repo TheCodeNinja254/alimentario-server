b856679633352742fc19affd96a91cb7e4031fb1
fredrick <fmmbugua@safaricom.co.ke>
2022-07-15 12:43:15 +0300

    Create gallery photos model

---
+++ src/models/GalleryPhoto.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../Database/connection");
+
+module.exports = sequelize.define("GalleryPhoto", {
+  id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  photoUri: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    unique: true,
+  },
+  photoDescription: {
+    type: Sequelize.STRING(255),
+  },
+  photoName: {
+    type: Sequelize.STRING(255),
+  },
+  photoPreferredLocation: {
+    type: Sequelize.STRING(50),
+  },
+  photoTag: {
+    type: Sequelize.STRING(30),
+  },
+  photoBy: {
+    type: Sequelize.STRING(30),
+  },
+  updatedBy: Sequelize.STRING(30),
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
+});
+
+// sequelize migration:create --name create_gallery_photos_table
