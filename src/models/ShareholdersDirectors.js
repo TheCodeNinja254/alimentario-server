87db1c9985cd64cd01f5f2ea9e2ad453cfc4621d
fredrick <fmmbugua@safaricom.co.ke>
2022-07-15 12:27:29 +0300

    Create shareholders and directors model

---
+++ src/models/ShareholdersDirectors.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../Database/connection");
+
+module.exports = sequelize.define("ShareholdersDirector", {
+    id: {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    businessId: {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+    },
+    memberName: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+    },
+    memberType: {
+        type: Sequelize.STRING(30),
+    },
+    updatedBy: Sequelize.STRING(30),
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+});
+
+// sequelize migration:create --name create_shareholders_directors_table
+
